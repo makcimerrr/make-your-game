87d55954e9fd373aaae1ee01f8453cf076f46e4b
rclatot <romainclatotpro.proton.me>
2023-12-21 11:03:03 +0100

    push ajoute boutton plus son debut du jeu

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,7 @@
-var clickSound = new Audio("audio/shotgun.mp3"); // Remplacez 'click-sound.mp3' par le chemin de votre fichier son
+var clickSound = new Audio("audio/shotgun.mp3");
 
 document.getElementById("startButton").addEventListener("click", function () {
-  // Add code to start your game here
-  alert("Game started!");
-
-  // For example, you could redirect to the game page or hide/show elements to start the game
-  // window.location.href = 'game.html';
+  window.location.href = "html/game.html";
 });
 
 document.addEventListener("click", function () {
