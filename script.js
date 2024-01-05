be6675cffcd32a781e8cb7cfec050d65448ceed8
efemenia <enzofemenia1@gmail.com>
2024-01-05 17:33:42 +0100

    bah c'est bientot fini wlh

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
 
+//////////////menu//////////////////
+
+////////////recup divs//////////////
+const menu = document.getElementById('menu')
+const grass = document.querySelector('.grass');
+const dog = document.querySelector('.dog')
+////////////recup divs//////////////
+
+
+function startGame() {
+  setInterval(function () {
+    moveDuck(ducks);
+  }, speed);
+  startTime = Date.now();
+  menuDisapearInGame()
+dog.classList.replace('snif', 'find')
+  setTimeout(() => {
+    startCreatingDucks();
+    setInterval(getRandomValue, 1000);
+  },1000)
+}
+
+
+
+function menuDisapearInGame() { //fait disparaitre le menu quand on clique sur start
+  console.log('menu disapear')
+  menu.style.opacity = 0
+  grass.classList.toggle('paused');
+  
+  setTimeout(() => {
+    menu.style.zIndex = -999;
+  }, 500);
+}
+
+function showCredits() {
+  console.log('Artworks : Enzo')
+  console.log('Front-End : Enzo')
+  console.log('Back-End : Enzo ft. Chat-Gpt')
+}
+
+
+
+
+
+//////////////game//////////////////
+
 const gameContainer = document.getElementById('game-container');
 let duckCount = 0;
 const ducks = [];
 
 let i = 0;
 var maxDuck = 16;
-var duckStartHeight = 2000
+var duckStartHeight = 2500
 
 var distance = 16;
-var speed = 70;
+var speed = 100;
 
 const duckWidth = 66;
 const duckHeight = 66;
@@ -16,28 +62,27 @@ const duckHeight = 66;
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
 
+let randomValues = [];
+
+let blueCount = 1;
+let redCount = 2;
+
 window.addEventListener('resize', () => {
   screenWidth = window.innerWidth;
   screenHeight = window.innerHeight;
 });
 
-let randomValues = [];
 
-startTime = Date.now();
 function getRandomValue() {
   randomValues = [];
   const currentTime = Date.now() - startTime;
   ducks.forEach(() => {
     const leftOrRightValue = Math.floor(Math.random() * 3);
-    const upOrDownValue = currentTime < duckStartHeight ? 2 : Math.floor(Math.random() * 3);
-    
+    const upOrDownValue = currentTime < (duckStartHeight+1000) ? 2 : Math.floor(Math.random() * 3);
     randomValues.push({ leftOrRightValue, upOrDownValue });
   });
-  console.log(randomValues);
 }
 
-let blueCount = 1;
-let redCount = 2;
 
 function isBlackBlueOrRed() {
   if (blueCount > 0 && Math.random() < 0.05) {
@@ -66,8 +111,21 @@ function createDuck(id, initialX, initialY) {
     duck.classList.add('blue');
   }
 
+  duck.addEventListener('click', function () {
+    duck.classList.add('dead')
+    console.log('detruit')
+    /*     removeDuck(duck); */
+  });
   gameContainer.appendChild(duck);
   ducks.push(duck);
+}
+
+function removeDuck(duck) {
+  duck.remove();
+  const index = ducks.indexOf(duck);
+  if (index !== -1) {
+    ducks.splice(index, 1);
+  }
 }
 
 function startCreatingDucks() {
@@ -75,34 +133,38 @@ function startCreatingDucks() {
   setInterval(getRandomValue());
   i++;
   if (i < maxDuck) {
-    setTimeout(startCreatingDucks,70);
+    setTimeout(startCreatingDucks, 100);
   }
 }
-startCreatingDucks();
+
 
 function moveDuck(ducks) {
   ducks.forEach((duck, index) => {
-    const maxX = gameContainer.offsetWidth - duckWidth;
-    const maxY = gameContainer.offsetHeight - duckHeight;
+    /*     const maxX = gameContainer.offsetWidth - duckWidth;
+        const maxY = gameContainer.offsetHeight - duckHeight; */
 
     let newX = parseInt(duck.style.left);
     let newY = parseInt(duck.style.top);
 
     const { leftOrRightValue, upOrDownValue } = randomValues[index];
-
-    if (leftOrRightValue === 0) {
-      newX = Math.max(newX - distance, /* 0 */);
-    } else if (leftOrRightValue === 2) {
-      newX = Math.min(newX + distance, /* maxX */);
-    }
-    if (upOrDownValue === 0) {
-      newY = Math.min(newY + distance,/*  maxY  */);
-    } else if (upOrDownValue === 2) {
-      newY = Math.max(newY - distance, /* 0 */);
-    }
-    if (leftOrRightValue === 1 && upOrDownValue === 1) {
-      newY -= 5;
-      newX += 10;
+    if (duck.classList.contains('dead')) {
+      newX += 0
+      newY += 0
+    } else {
+      if (leftOrRightValue === 0) {
+        newX = Math.max(newX - distance, /* 0 */);
+      } else if (leftOrRightValue === 2) {
+        newX = Math.min(newX + distance, /* maxX */);
+      }
+      if (upOrDownValue === 0) {
+        newY = Math.min(newY + distance,/*  maxY  */);
+      } else if (upOrDownValue === 2) {
+        newY = Math.max(newY - distance, /* 0 */);
+      }
+      if (leftOrRightValue === 1 && upOrDownValue === 1) {
+        newY -= 5;
+        newX += 10;
+      }
     }
 
     const transformations = {
@@ -117,12 +179,18 @@ function moveDuck(ducks) {
       '1-1': ['flying-right', `scaleY(1) rotate(0deg)`]
     };
 
+
+
     const key = `${leftOrRightValue}-${upOrDownValue}`;
     if (transformations[key]) {
       const [className, transformValue] = transformations[key];
       duck.classList.remove('flying-top', 'flying-top-right', 'flying-right');
-      duck.classList.add(className);
-      duck.style.transform = transformValue;
+      if(!duck.classList.contains('dead')) {
+        duck.classList.add(className);
+        duck.style.transform = transformValue;
+      } else {
+        duck.style.transform = `scaleY(1) rotate(0deg)`
+      }
     }
 
     duck.style.left = newX + 'px';
@@ -130,8 +198,3 @@ function moveDuck(ducks) {
   });
 }
 
-setInterval(function () {
-  moveDuck(ducks);
-}, speed);
-
-setInterval(getRandomValue, 1000);
